43aca5cdd11589968c5b2b483c7ae1693de658d2
jcanizalez <javier-canizalez@outlook.com>
2022-02-21 10:39:33 -0600

    Renamed main folder

--- variablegroupformatter/index.ts
+++ variablegroupformatter/index.ts
@@ -1,4 +1,4 @@
-import tl = require('azure-pipelines-task-lib/task');
+import tl = require('azure-pipelines-task-lib');
 import * as azdev from "azure-devops-node-api";
 import * as ta from "azure-devops-node-api/TaskAgentApi";
 import * as ti from "azure-devops-node-api/interfaces/TaskAgentInterfaces";
